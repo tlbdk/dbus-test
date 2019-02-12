251ae5f1d3979f7fcea51750db2c654c3392e2d2
Troels Liebe Bentsen <tlb@nversion.dk>
2019-02-12 22:18:21 +0100

    Get sample working

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # dbus-test
+
 Small test repo to talk to dbus from Node
+
+Port/socket forward with SSH to a remote servers system dbus:
+
+``` bash
+ssh -xL $(pwd)/system_bus_socket:/run/dbus/system_bus_socket user@server
+```
+
+Same with TCP:
+
+``` bash
+socat -v TCP-LISTEN:12345,reuseaddr,fork UNIX-CONNECT:/run/dbus/system_bus_socket
+```
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const dbus = require('dbus-native')
 
 //process.env.DBUS_SYSTEM_BUS_ADDRESS = `tcp:host=localhost,port=12345`
-//process.env.DBUS_SYSTEM_BUS_ADDRESS = `unix:path=${__dirname}/../system_bus_socket`
+process.env.DBUS_SYSTEM_BUS_ADDRESS = `unix:path=${__dirname}/../system_bus_socket`
 
 // Overwrite the uid for dbus authentication
 process.getuid = () => {
-  return 0
+  return 1000
 }
 
 const systemBus = dbus.systemBus()
@@ -14,3 +14,21 @@ systemBus.listNames((err, notifications) => {
   console.log(JSON.stringify(err))
   console.log(JSON.stringify(notifications))
 })
+
+systemBus
+  .getService('org.freedesktop.login1')
+  .getInterface(
+    '/org/freedesktop/login1',
+    'org.freedesktop.login1.Manager',
+    (err, loginManager) => {
+      if (err) {
+        return console.error(err)
+      }
+      loginManager.on('SessionNew', (sessionId, sessionPath) => {
+        console.log(`open session ${sessionId}: ${sessionPath}`)
+      })
+      loginManager.on('SessionRemoved', (sessionId, sessionPath) => {
+        console.log(`close session ${sessionId}: ${sessionPath}`)
+      })
+    }
+  )
